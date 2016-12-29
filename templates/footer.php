b562d8fe0fa1608cb28f0938f9bc4e1b5b0eebe0
Sandip Karanjekar <sandipkaranjekar@gmail.com>
2016-12-29 23:39:56 +0530

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <hr />
 <div class="span12">
-	<center><p>Micro CRM &copy; 2014 Powered by <a href="http://www.techschema.com" target="_blank">Tech Schema</a></p></center>
+	<center><p> &copy; 2014 Powered by <a href="http://www.techschema.com" target="_blank">Tech Schema</a></p></center>
 </div>
 
